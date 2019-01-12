ea6ba5c8a2a82e641fe111c18d89b1686819073d
Alexandr Rogovoy <rogovoi200023507@gmail.com>
2019-01-12 13:01:18 +0300

    Now for queries in MessageRepositoryImpl we use MappingSqlQuery<T> class

--- backend/src/main/java/com/alrgv/messageservice/backend/repository/impl/MessageRepositoryImpl.java
+++ backend/src/main/java/com/alrgv/messageservice/backend/repository/impl/MessageRepositoryImpl.java
@@ -8,99 +8,122 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.support.PageableExecutionUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.PreparedStatementSetter;
-import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.object.MappingSqlQuery;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class MessageRepositoryImpl implements MessageRepository {
 
+    private static final String SQL_SELECT_BY_ID_UN_PAGEABLE = "SELECT * FROM message m " +
+            "INNER JOIN user fromUser on m.from_user = fromUser.id " +
+            "INNER JOIN user toUser on m.to_user = toUser.id " +
+            "WHERE fromUser.id = :uId";
+
+    private static final String SQL_SELECT_BY_ID_PAGEABLE = "SELECT * FROM message m " +
+            "INNER JOIN user fromUser on m.from_user = fromUser.id " +
+            "INNER JOIN user toUser on m.to_user = toUser.id " +
+            "WHERE fromUser.id = :uId LIMIT :off, :cnt";
+
+    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
+    private MappingSqlQueryImpl findByIdPageable, findByIdUnPageable;
 
     @Autowired
     public MessageRepositoryImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+
+        this.findByIdPageable = new MappingSqlQueryImpl(dataSource,
+                SQL_SELECT_BY_ID_PAGEABLE,
+                new SqlParameter("uId", Types.BIGINT),
+                new SqlParameter("off", Types.BIGINT),
+                new SqlParameter("cnt", Types.BIGINT));
+
+        this.findByIdUnPageable = new MappingSqlQueryImpl(dataSource,
+                SQL_SELECT_BY_ID_UN_PAGEABLE,
+                new SqlParameter("uId", Types.BIGINT));
     }
 
     @Override
     public Long count(@NonNull Long id) {
-        return jdbcTemplate
-                .queryForObject("SELECT COUNT(*) FROM message m " +
-                        "INNER JOIN user fromUser on m.from_user = fromUser.id " +
-                        "WHERE fromUser.id = " + id, Long.class);
+        try {
+            PreparedStatement countStatement =
+                    dataSource
+                            .getConnection()
+                            .prepareStatement("SELECT COUNT(*) FROM message m " +
+                                    "INNER JOIN user fromUser on m.from_user = fromUser.id " +
+                                    "WHERE fromUser.id = ?");
+            countStatement.setLong(1, id);
+            ResultSet rs = countStatement.executeQuery();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            return 0L;
+        }
     }
 
+    /**
+     * {@link org.springframework.data.repository.support.PageableExecutionUtils}
+     * {@link org.springframework.data.jpa.repository.support.SimpleJpaRepository} 331 row
+     */
     @Override
     public Page<Message> findMessagesByUserId(@NonNull Pageable pageable, @NonNull Long id) {
-        ResultSetExtractor<List<Message>> resultSetExtractor =
-                rs -> {
-                    ArrayList<Message> messages = new ArrayList<>();
-                    while (rs.next()) {
-                        Message message = new Message(
-                                rs.getLong("id"),
-                                new Account(rs.getInt(11),
-                                        rs.getString(12),
-                                        rs.getString(13)),
-                                new Account(rs.getInt(14),
-                                        rs.getString(15),
-                                        rs.getString(16)),
-                                rs.getTimestamp("sent"),
-                                rs.getString("subject"),
-                                rs.getString("message_text"),
-                                rs.getInt("send_status"),
-                                rs.getBoolean("read_status"),
-                                rs.getBoolean("starred"),
-                                rs.getBoolean("important")
-                        );
-                        messages.add(message);
-                    }
-                    return messages;
-                };
+        Map<String, Object> params = new HashMap<>();
+        params.put("uId", id);
+        if (pageable.isPaged()) {
+            params.put("off", pageable.getOffset());
+            params.put("cnt", pageable.getPageSize());
+        }
 
-/**
- * {@link org.springframework.data.repository.support.PageableExecutionUtils}
- * {@link org.springframework.data.jpa.repository.support.SimpleJpaRepository} 331 row
- */
-        List<Message> userMessages =
-                jdbcTemplate.query(getCreator(pageable),
-                        getSetter(pageable, id),
-                        resultSetExtractor);
-        if (userMessages == null)
-            userMessages = Collections.emptyList();
+        MappingSqlQueryImpl query = pageable.isPaged() ? findByIdPageable : findByIdUnPageable;
+        List<Message> userMessages = query.executeByNamedParam(params);
 
         return PageableExecutionUtils.getPage(userMessages, pageable, () -> count(id));
     }
 
-    private PreparedStatementCreator getCreator(Pageable pageable) {
-        if (!pageable.isPaged()) {
-            return connection -> connection
-                    .prepareStatement("SELECT * FROM message m " +
-                            "INNER JOIN user fromUser on m.from_user = fromUser.id " +
-                            "INNER JOIN user toUser on m.to_user = toUser.id " +
-                            "WHERE fromUser.id = ? ");
-        } else {
-            return connection -> connection
-                    .prepareStatement("SELECT * FROM message m " +
-                            "INNER JOIN user fromUser on m.from_user = fromUser.id " +
-                            "INNER JOIN user toUser on m.to_user = toUser.id " +
-                            "WHERE fromUser.id = ? LIMIT ?, ?");
-        }
-    }
+    private class MappingSqlQueryImpl extends MappingSqlQuery<Message> {
 
-    private PreparedStatementSetter getSetter(Pageable pageable, Long id) {
-        return preparedStatement -> {
-            preparedStatement.setLong(1, id);
-            if (pageable.isPaged()) {
-                preparedStatement.setLong(2, pageable.getOffset());
-                preparedStatement.setLong(3, pageable.getPageSize());
-            }
-        };
+        MappingSqlQueryImpl(DataSource ds, String sql) {
+            setDataSource(ds);
+            setSql(sql);
+            afterPropertiesSet();
+        }
+
+        MappingSqlQueryImpl(DataSource ds, String sql, SqlParameter... parameters) {
+            setDataSource(ds);
+            setSql(sql);
+            setParameters(parameters);
+            afterPropertiesSet();
+        }
+
+        @Override
+        protected Message mapRow(ResultSet rs, int i) throws SQLException {
+            return new Message(
+                    rs.getLong("id"),
+                    new Account(rs.getInt(11),
+                            rs.getString(12),
+                            rs.getString(13)),
+                    new Account(rs.getInt(14),
+                            rs.getString(15),
+                            rs.getString(16)),
+                    rs.getTimestamp("sent"),
+                    rs.getString("subject"),
+                    rs.getString("message_text"),
+                    rs.getInt("send_status"),
+                    rs.getBoolean("read_status"),
+                    rs.getBoolean("starred"),
+                    rs.getBoolean("important")
+            );
+        }
     }
 }
